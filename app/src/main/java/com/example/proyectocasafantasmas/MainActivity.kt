1f7384c4835240fc92d5eb1488510e69302b8c11
estelaV9 <esteladeveg@gmail.com>
2024-12-04 17:47:43 +0100

    añadir el mibinding

--- app/src/main/java/com/example/proyectocasafantasmas/MainActivity.kt
+++ app/src/main/java/com/example/proyectocasafantasmas/MainActivity.kt
@@ -5,8 +5,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.proyectocasafantasmas.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var mibinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
